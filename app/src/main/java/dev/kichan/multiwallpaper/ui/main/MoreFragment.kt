0ade50d3be0f42c22dde98e120678ca239d15a16
바키찬 <ckstmznf@naver.com>
2023-07-11 02:13:49 +0900

    Developer donations

--- app/src/main/java/dev/kichan/multiwallpaper/ui/main/MoreFragment.kt
+++ app/src/main/java/dev/kichan/multiwallpaper/ui/main/MoreFragment.kt
@@ -1,18 +1,33 @@
 package dev.kichan.multiwallpaper.ui.main
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import dev.kichan.multiwallpaper.BaseFragment
 import dev.kichan.multiwallpaper.R
+import dev.kichan.multiwallpaper.databinding.FragmentMoreBinding
 
-class MoreFragment : Fragment() {
+class MoreFragment : BaseFragment<FragmentMoreBinding>(R.layout.fragment_more) {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
 
-        return inflater.inflate(R.layout.fragment_more, container, false)
+        binding.run {
+            btnMoreAppInfo.setOnClickListener {
+                val url = "http://www.toss.me/바키찬희찬"
+                val intent = Intent(Intent.ACTION_VIEW).apply {
+                    data = Uri.parse(url)
+                }
+                startActivity(intent)
+            }
+        }
+
+        return binding.root
     }
 }
